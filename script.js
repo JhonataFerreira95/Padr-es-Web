6817887016635bb326c91ad838327c60653a7790
Bass <jhonataovirowa@gmail.com>
2024-07-08 19:23:53 -0300

    ajustado o botão

---
+++ script.js
@@ -0,0 +1,15 @@
+const botaoalterartema = document.getElementById("botao-alterar-tema");
+const body = document.querySelector("body");
+const imagembotatrocadetema = document.querySelector(".botao-alterar-tema")
+
+botaoalterartema.addEventListener("click", () => {
+    const modoescuroestaativo = body.classList.contains("modo-escuro")
+
+    body.classList.toggle("modo-escuro")
+console.log("aqui");
+    if (modoescuroestaativo) {
+        imagembotatrocadetema.setAttribute("src", "./black-hole.svg")
+    } else {
+        imagembotatrocadetema.setAttribute("src", "./black-hole (1).svg")
+    }
+});
